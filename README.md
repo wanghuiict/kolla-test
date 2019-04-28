c1fa8da5b71755b47d89546acca6652dec79f894
wanghuiict <wanghuiict@gmail.com>
2019-04-28 15:41:44 +0800

    print-docker-registry.sh

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-#test opensatck kolla
+# test opensatck kolla
+
+## update private registry taglist
+
+```
+./print-docker-registry.sh  > registry.md
+git add registry.md && git commit -m "update regisrty.md"
+git push origin master
+```
 
---
+++ print-docker-registry.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERN="username"
+PASSW="password"
+HOST1="1.2.3.4"
+PORT1="5000"
+
+. .secret
+
+echo -e "# Docker Registry\n\n## repositories"
+
+repolist=($(echo "
+catal=$(curl -s -k https://$USERN:$PASSW@$HOST1:$PORT1/v2/_catalog)
+for repo in catal['repositories']:
+    print repo
+" |python -))
+
+for x in ${repolist[@]}; do
+    echo "
+taglist=$(curl -s -k https://$USERN:$PASSW@$HOST1:$PORT1/v2/$x/tags/list)
+print('### %s'%taglist['name'])
+for t in taglist['tags']:
+  print('%s\n'%(t))
+" |python -
+done
+
