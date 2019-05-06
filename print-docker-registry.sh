047088a0f1136a28ced917fc147741062730aa71
wanghuiict <wanghuiict@gmail.com>
2019-05-06 08:46:47 +0800

    print docker registry include pagination

--- print-docker-registry.sh
+++ print-docker-registry.sh
@@ -2,20 +2,36 @@
 
 # output markdown document
 
-echo -e "# Docker Registry\n\n## repositories\nhttps://$HOST1:$PORT1/v2/"
+last=
 
-repolist=($(echo "
-catal=$(curl -s -k https://$USERN:$PASSW@$HOST1:$PORT1/v2/_catalog)
+echo -e "\n# Docker Registry\n\n## repositories\nhttps://$HOST1:$PORT1/v2/"
+
+while true; do
+        if [ ! -z "$last" ]; then
+            lstr="?last=$last"
+        else
+            lstr=
+        fi
+        
+        repolist=($(echo "
+catal=$(curl -s -k https://$USERN:$PASSW@$HOST1:$PORT1/v2/_catalog$lstr)
 for repo in catal['repositories']:
     print repo
 " |python -))
-
-for x in ${repolist[@]}; do
-    echo "
+        
+        if [ ${#repolist[@]} -eq 0 ]; then
+            break
+        fi
+        
+        for x in ${repolist[@]}; do
+            echo "
 taglist=$(curl -s -k https://$USERN:$PASSW@$HOST1:$PORT1/v2/$x/tags/list)
 print('### %s'%taglist['name'])
 for t in taglist['tags']:
   print('%s\n'%(t))
 " |python -
+        done
+        
+        last=$(echo ${x//\//%2F})
+        ##echo $last
 done
-
