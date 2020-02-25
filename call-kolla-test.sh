0f77d9c2286c68e73de5bce3a5e6fc9da58f9856
wanghuiic <wanghuiict@sina.com>
2020-02-25 15:13:44 +0800

    call-kolla-test.sh allows adding kolla options

--- call-kolla-test.sh
+++ call-kolla-test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 show_help_install() {
-        echo -e "test kolla.\nUsage:\n\t$myname [options]\nOptions:"
+        echo -e "test kolla.\nUsage:\n\t$myname [options] <action> [ansible-playbook options...]"
+        echo -e "Options:"
         echo -e "\t--help, -h              \tshow help."
         echo -e "\t--skip-install-kolla    \tskip install kolla."
+        echo -e "Parameters:"
+        echo -e "\taction                  \tkolla_acion: bootstrap-servers,prechecks,deploy,post-deploy"
+        echo -e "Example:"
+        echo -e "\t$myname --skip-install-kolla deploy"
+        echo -e "\t$myname --skip-install-kolla deploy --tags=mariadb"
+        echo -e "\t$myname --skip-install-kolla deploy --tags=mariadb --list-tasks --list-tags"
 }
 
 cmdopts=$(getopt --longoptions skip-install-kolla,title:,help \
@@ -39,6 +46,17 @@ while true; do
   esac
 done
 
+action=$1
+shift
+if [ -z "$action" ]; then
+    echo "no kolla_action found" >&1
+    exit 1
+fi
+if [[ "$action" != "bootstrap-servers" && "$action" != "prechecks" && "$action" != "deploy" && "$action" != "post-deploy" ]]; then
+    echo "error action" >&1
+    exit 1
+fi
+
 if $skip_install_kolla; then
     . tn-openstack-common --skip-install-kolla
 else
@@ -47,4 +65,7 @@ fi
 
 confd=$ETCKOLLA
 kollad=$KOLLADIR
-ansible-playbook --tags=mariadb --list-tasks --list-tags -i $confd/multinode -e @$confd/globals.yml -e @$confd/passwords.yml -e CONFIG_DIR=$confd  -e kolla_action=deploy $kollad/ansible/site.yml
+
+echo "ansible-playbook -i $confd/multinode -e @$confd/globals.yml -e @$confd/passwords.yml -e CONFIG_DIR=$confd -e kolla_action=$action $@ $kollad/ansible/site.yml"
+ansible-playbook -i $confd/multinode -e @$confd/globals.yml -e @$confd/passwords.yml -e CONFIG_DIR=$confd -e kolla_action=$action $@ $kollad/ansible/site.yml
+
